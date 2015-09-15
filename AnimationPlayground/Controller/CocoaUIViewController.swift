a602ff5fd2d17bf53a2672f9e1c0952fe843d467
sjcode <su.jian@me.com>
2015-09-15 13:08:08 +0800

    first commit

--- AnimationPlayground/Controller/CocoaUIViewController.swift
+++ AnimationPlayground/Controller/CocoaUIViewController.swift
@@ -18,14 +18,15 @@ class CocoaUIViewController: UIViewController,UITextFieldDelegate {
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        
-        var animation = CAKeyframeAnimation()
-        animation.keyPath = "position.x"
-        animation.values = [0,10,-10,10,0]
-        animation.keyTimes = [0, 1/6.0, 3/6.0, 5/6.0, 1]
-        animation.duration = 0.4
-        animation.additive = true
-        self.passwordPanel.layer.addAnimation(animation, forKey: nil)
+        if textField == passwordTextFiled{
+            var animation = CAKeyframeAnimation()
+            animation.keyPath = "position.x"
+            animation.values = [0,10,-10,10,0]
+            animation.keyTimes = [0, 1/6.0, 3/6.0, 5/6.0, 1]
+            animation.duration = 0.4
+            animation.additive = true
+            self.passwordPanel.layer.addAnimation(animation, forKey: nil)
+        }
         return true
     }
     
---
+++ AnimationPlayground/Controller/RotationViewController.swift
@@ -0,0 +1,48 @@
+//
+//  RotationViewController.swift
+//  AnimationPlayground
+//
+//  Created by sujian on 15/9/12.
+//  Copyright (c) 2015年 sujian. All rights reserved.
+//
+
+import UIKit
+
+class RotationViewController: UIViewController {
+
+    @IBOutlet weak var containView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let shipLayer = CALayer()
+        shipLayer.frame = CGRectMake(0, 0, 128, 128)
+        shipLayer.position = CGPointMake(150, 150)
+        shipLayer.contents = UIImage(named: "snowman")?.CGImage
+        
+        self.containView.layer .addSublayer(shipLayer)
+        
+        let animation1 = CABasicAnimation()
+        animation1.keyPath = "transform.rotation"
+        animation1.duration = 2
+        animation1.toValue = 2 * M_PI
+        animation1.repeatCount = HUGE
+        //shipLayer.addAnimation(animation1, forKey: nil)
+        
+        let animation2 = CABasicAnimation()
+        animation2.keyPath = "transform.scale"
+        animation2.toValue = 2
+        animation2.duration = 2
+        animation2.repeatCount = HUGE
+        //shipLayer.add
+
+        let animationGroup = CAAnimationGroup()
+        animationGroup.animations = [animation1,animation2]
+        animationGroup.duration = 4
+        animationGroup.autoreverses = true
+        animationGroup.repeatCount = HUGE
+        shipLayer.addAnimation(animationGroup, forKey: nil)
+    }
+
+    
+
+}
